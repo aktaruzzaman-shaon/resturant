460c71abbd733c4bf51670c9521944f04727437b
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2022-12-30 12:42:16 +0600

    adding all servies

--- src/Component/Home/Services/Services/Service.js
+++ src/Component/Home/Services/Services/Service.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import service1 from '../../../../images/Sevice/service1.png'
+import service2 from '../../../../images/Sevice/Service2.png'
+import service3 from '../../../../images/Sevice/Service3.png'
 
 const Service = () => {
     return (
@@ -8,20 +10,51 @@ const Service = () => {
                 <h2>Why you choose us</h2>
                 <p>Barton waited tewnty always repair in within we do. An delightes offending curiosity my is dashwoods st . prosperous incresasding surrounded.</p>
             </div>
-            <div>
-                <div className="img">
-                    <img src={service1} alt="" />
-                </div>
-                <div className="service-description">
-                    <h4>Fast Delivery</h4>
-                    <p>
-                        Barton waited tewnty always repair in within we do. An delightes offending curiosity my is dashwoods st . prosperous incresasding surrounded.
-                    </p>
+            <div className="serviceDetail d-flex container">
+                <div>
+                    <div className="img">
+                        <img className='w-75' src={service1} alt="" />
+                    </div>
+                    <div className="service-description">
+                        <h4>Fast Delivery</h4>
+                        <p>
+                            Barton waited tewnty always repair in within we do. An delightes offending curiosity my is dashwoods st . prosperous incresasding surrounded.
+                        </p>
+                    </div>
+                    <div>
+                        <span>See more</span>
+                    </div>
                 </div>
                 <div>
-                    <span>See more</span>
+                    <div className="img">
+                        <img className='w-75' src={service2} alt="" />
+                    </div>
+                    <div className="service-description">
+                        <h4>A Good Auto Responder</h4>
+                        <p>
+                            Barton waited tewnty always repair in within we do. An delightes offending curiosity my is dashwoods st . prosperous incresasding surrounded.
+                        </p>
+                    </div>
+                    <div>
+                        <span>See more</span>
+                    </div>
+                </div>
+                <div>
+                    <div className="img">
+                        <img className='w-75' src={service3} alt="" />
+                    </div>
+                    <div className="service-description">
+                        <h4>Home delivery</h4>
+                        <p>
+                            Barton waited tewnty always repair in within we do. An delightes offending curiosity my is dashwoods st . prosperous incresasding surrounded.
+                        </p>
+                    </div>
+                    <div>
+                        <span>See more</span>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
